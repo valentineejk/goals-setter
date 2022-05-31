cf6269a5f27c0609231630a9c906f6ddd2a30a70
valentineejk <valentineejk@gmail.com>
2022-05-31 10:26:58 +0100

    delete goal func

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,108 +1,91 @@
 const asyncHandler = require('express-async-handler')
-const goal = require('../models/goalModel')
+
+const Goal = require('../models/goalModel')
 const User = require('../models/userModel')
 
-
-// @desc Get Goals
-// @route Get /api/goals
-//@access Private
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
 const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find({ user: req.user.id })
 
-    const goals = await goal.find({ user: req.user.id })
     res.status(200).json(goals)
 })
 
-// @desc Set Goals
-// @route POST /api/goals
-//@access Private
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
 const setGoal = asyncHandler(async (req, res) => {
-
-
     if (!req.body.text) {
         res.status(400)
-        throw new Error('please add a text field')
+        throw new Error('Please add a text field')
     }
 
-    const goal = await goal.create({
+    const goal = await Goal.create({
         text: req.body.text,
-        user: req.user.id
-
+        user: req.user.id,
     })
 
-
     res.status(200).json(goal)
-
-
-
 })
 
-// @desc Update Goals
-// @route PUT /api/goals/:id
-//@access Private
+// @desc    Update goal
+// @route   PUT /api/goals/:id
+// @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-
-    const goal = await goal.findById(req.params.id)
+    const goal = await Goal.findById(req.params.id)
 
     if (!goal) {
         res.status(400)
-        throw new Error('Goal Not Found')
+        throw new Error('Goal not found')
     }
 
-    const user = await Goal.findByIdAndUpdate(req.params.id, req.body.body, {
+    // Check for user
+    if (!req.user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
     })
-
-    if (!user) {
-        res, status(401)
-        throw new Error('User Not Found')
-    }
-
-
-    if (goal.user.toString() !== user.id) {
-        res.status(401)
-        throw new Error('Not Authorized')
-    }
-
-    const updatedGoal = await goal.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true, })
 
     res.status(200).json(updatedGoal)
 })
 
-// @desc Delete Goals
-// @route DELETE /api/goals
-//@access Private
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-
-    const goal = await goal.findById(req.params.id)
+    const goal = await Goal.findById(req.params.id)
 
     if (!goal) {
         res.status(400)
-        throw new Error('Goal Not Found')
+        throw new Error('Goal not found')
     }
 
-    const user = await Goal.findByIdAndUpdate(req.params.id, req.body.body, {
-        new: true,
-    })
-
-    if (!user) {
-        res, status(401)
-        throw new Error('User Not Found')
-    }
-
-
-    if (goal.user.toString() !== user.id) {
+    // Check for user
+    if (!req.user) {
         res.status(401)
-        throw new Error('Not Authorized')
+        throw new Error('User not found')
     }
-    await goal.remove()
-    res.status(200).json({
-        id: req.params.id
-    })
-})
 
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({ id: req.params.id })
+})
 
 module.exports = {
     getGoals,
---
+++ frontend/src/features/goals/goalSlice.js
@@ -0,0 +1,113 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import goalApi from '../goals/goalApi'
+
+
+
+const initialState = {
+    goals: [],
+    isError: false,
+    isLoading: false,
+    isSuccess: false,
+    message: '',
+
+}
+
+//creategoal
+
+export const createGoal = createAsyncThunk('goals/create', async (goalData, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalApi.createGoal(goalData, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+//getGoal
+
+export const getGoals = createAsyncThunk('goals/getAll', async (_, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalApi.getGoals(token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+//creategoal
+
+export const deleteGoal = createAsyncThunk('goals/delete', async (id, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await goalApi.deleteGoal(id, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+export const goalSlice = createSlice({
+    name: 'goal',
+    initialState,
+    reducers: {
+        reset: (state) => initialState,
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createGoal.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(createGoal.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals.push(action.payload)
+            })
+            .addCase(createGoal.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+
+
+            .addCase(getGoals.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(getGoals.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals = action.payload
+            })
+            .addCase(getGoals.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+
+
+
+            .addCase(deleteGoal.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(deleteGoal.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals = state.goals.filter((goal) => goal._id !== action.payload.id)
+            })
+            .addCase(deleteGoal.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+
+    }
+})
+
+export const { reset } = goalSlice.actions
+
+export default goalSlice.reducer
